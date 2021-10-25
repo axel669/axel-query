ac7f2d530ae06af5ca83dd3347cde8160bce6331
Axel669 <haryuu.hanekata@gmail.com>
2021-10-24 20:53:53 -0700

    add simple tests for $ files

---
+++ tests/handlers/test/$finder.js
@@ -0,0 +1,22 @@
+const users = require("../../data/users")
+
+const userSearch = {
+    "value[]": {
+        "user": "string",
+        "perms[]": {
+            "name": "string"
+        }
+    },
+    args: {
+        "user": "string",
+        "pw": "string"
+    },
+    func: async (args, mask) => {
+        console.log(mask)
+        return users.filter(
+            user => user.user === args.user
+        )
+    }
+}
+
+module.exports = userSearch
